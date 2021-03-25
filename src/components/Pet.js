01a5507b8dc6ded4d17b1dc2d88139e7e097e932
Tim Flood <tflood9@gmail.com>
2021-03-25 12:34:02 -0400

    commit tree df40fe178999a1194a43d57b611c6393bd4d31b9

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 class Pet extends React.Component {
   render() {
@@ -6,15 +6,15 @@ class Pet extends React.Component {
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {this.props.pet.name}{" "}
+            {this.props.pet.gender === "female" ? "♀" : "♂"}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
@@ -22,8 +22,8 @@ class Pet extends React.Component {
           <button className="ui primary button">Adopt pet</button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Pet
+export default Pet;
